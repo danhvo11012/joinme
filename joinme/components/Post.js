3ecacedfd1ef3282c48187a8541ca23a0482bae5
danhvo11012 <danhvo11012@gmail.com>
2020-02-23 13:19:48 -0600

    Done post create/delete

--- joinme/components/Post.js
+++ joinme/components/Post.js
@@ -8,6 +8,7 @@ import {
   LayoutAnimation,
   UIManager,
   KeyboardAvoidingView,
+  ShadowPropTypesIOS,
 } from 'react-native';
 
 import {
@@ -21,7 +22,7 @@ import {
 
 import Comment from './Comment';
 
-export default function Post({post, postKey}) {
+function Post(props) {
 
   const [ viewCommentOn, setViewCommentOn ] = useState(false);
   const [ toggle, setToggle ] = useState(false);
@@ -34,26 +35,51 @@ export default function Post({post, postKey}) {
     content: "This is the new comment from someOne."
   }
 
-  function handleSetToggle() {
+  const handleSetToggle = () => {
     setToggle(!toggle);
   }
 
+  const handleDeletePost = () => {
+    props.postToDelete(props.post._id);
+  }
+
   return(
-    <Card title={post.ownerEmail}>
+    <Card title={props.post.ownerEmail} containerStyle={{ width: '100%', alignSelf: 'center' }}>
       <View>
-        <Text>Post id: {post._id.toString()}</Text>
-        <Text>Post#: {postKey}</Text>
-        <Text>Post owner Id: {post.ownerId}</Text>
-        <Text>Post owner Email: {post.ownerEmail}</Text>
-        <Text>Post date: {post.postDate.toString()}</Text>
-        <Text>Post content:
-          <Text>{post.postContent}</Text>
-        </Text>
+        <Text>Post id: {props.post._id.toString()}</Text>
+        <Text>Post#: {props.postKey}</Text>
+        <Text>Post owner Id: {props.post.ownerId}</Text>
+        <Text>Post date: {props.post.postDate.toString()}</Text>
+        <Text>Category: {props.post.category}</Text>
+        <Text>Post content:     {props.post.content}</Text>
+        <Text>Preferred experience:     {props.post.preferred}</Text>
+      </View>
+
+        <View style={{ flexDirection: 'row', marginVertical: 10 }}>
+
+          <View style={{ flexGrow: 1, alignItems: 'flex-start'}} >
+            <Button style={{ width: 100}} title="Comment" type="outline" onPress={() => { handleSetToggle(); alert('Toggle view comment = ' + toggle)}} />
+          </View>
+
+          <View style={{ flexGrow: 1, alignItems: 'flex-end'}} >
+            <Button style={{ width: 50}} type="outline" 
+              icon={
+                <Icon
+                  name="delete"
+                  size={20}
+                />
+              }
+
+              onPress={handleDeletePost}
+            />
+              
+          </View>
         </View>
-        <Button style={{width: 100}} title="Comment" type="clear" onPress={() => { handleSetToggle(); alert('Toggle view comment = ' + toggle)}} />
+
         <Divider style={{ backgroundColor: 'black' }} />
-        <Comment post={post} postKey={postKey} comment={sampleComment} />
-    </Card>
-    
+        {/* <Comment post={props.post} postKey={props.postKey} comment={sampleComment} /> */}
+    </Card>   
   );
 }
+
+export default Post;
--- joinme/screens/MyDeskScreen.js
+++ joinme/screens/MyDeskScreen.js
@@ -11,9 +11,12 @@ import {
   UIManager,
   KeyboardAvoidingView,
   ScrollView,
+  Modal,
 } from 'react-native';
+
 import Post from '../components/Post'
 import { Input, Button, Icon } from 'react-native-elements';
+import CreatePostModal from '../components/CreatePostModal';
 
 
 function MyDeskScreen({ route, navigation }) {
@@ -25,49 +28,140 @@ function MyDeskScreen({ route, navigation }) {
   const { currentUserId, user } = route.params;
 
   const [ loadingComplete, setLoadingComplete] = useState(false);
+  const [ shouldReload, setShouldReload ] = useState(true );
+  const [ isPostReceived, setIsPostReceived ] = useState(false);
+  const [ isPostReady, setIsPostReady ] = useState(false);
+
   const [ myPosts, setMyPosts] = useState(null);
+  const [ showCreateNewPostModal, setShowCreateNewPostModal ] = useState(false);
+  const [ postFromModal, setPostFromModal ] = useState(null);
+  const [ fullPost, setFullPost ] = useState(null);
+
+  
+  const createNewPost = () => {
+    setShowCreateNewPostModal(true);
+  }
+
+  const handlePost = (modalShow) => {
+    setShowCreateNewPostModal(modalShow);
+  }
+
+  const handleGetPost = (postFromModal) => {
+    console.log('isPostReceived ', isPostReceived);
+    setPostFromModal(postFromModal);
+    setIsPostReceived(true);
+  }
+
+  const handleDeletePost = (postId) => {
+    posts.findOneAndDelete({_id: postId})
+      .then(res => {
+        console.log('response from delete: ', res);
+        setShouldReload(true);
+      })
+  }
+
+  const getPostToDelete = (postIdFromPostJS) => {
+    console.log('Post id to delete: ', postIdFromPostJS);
+    handleDeletePost(postIdFromPostJS);
+  }
+
+  /**
+  * Post metrics
+  * 
+  * postId = default ObjId
+  * ownerId = currentUserId
+  * ownerEmail = user.email
+  * category : Side-project || p2p learning || others
+  * postDate = new Date();
+  * postContent: TextInput
+  * preferred: Preferred exp
+  */
+  const handleSetFullPost = () => {
+    if(isPostReceived) {
+      setFullPost({
+        ownerId: currentUserId,
+        ownerEmail: user.email,
+        postDate: new Date(),
+        category: postFromModal.category,
+        content: postFromModal.content,
+        preferred: postFromModal.preferred,
+      });
+    }
+  }
+
+  const resetLogicStates = () => {
+    setShouldReload(false);
+    setIsPostReady(false);
+    setIsPostReceived(false);
+  }
 
   useEffect(()=> {
-
-
-    function handleSetPosts(results) {
-      setMyPosts(results);
-    }
-
-    posts.find({})
-      .toArray()
-      .then(results => {
-        handleSetPosts(results);
-        setLoadingComplete(true);
-      });
-  }, []);
-    
+    if(shouldReload) {
+      function handleSetPosts(results) {
+        setMyPosts(results);
+      }
   
+      posts.find({ ownerId: currentUserId })
+        .toArray()
+        .then(results => {
+          results.sort((a, b) => a._id < b._id);  // Sort results by document's id
+          handleSetPosts(results);
+          resetLogicStates();
+          setLoadingComplete(true);
+        });  
+    }
+  }, [shouldReload]);
 
-  function createNewPost() {
-    alert('Creating new post alert!');
-    console.log()
-  }
+  useEffect(() => {
+    if (isPostReceived) {
+      console.log('received');
+      console.log(postFromModal);
+      handleSetFullPost();
+      setIsPostReady(true);
+    }
+  }, [isPostReceived])
+
+  useEffect(() => {
+    if (isPostReady) {
+      console.log('Full post: ', fullPost);
+      posts.insertOne(fullPost).then(res => {
+        console.log('Response from stitch: ', res);
+        setLoadingComplete(false);
+        setShouldReload(true);
+      })
+    }
+  }, [isPostReady])
+
 
   if (!loadingComplete) { return null } 
   else {
     return(
       <ScrollView>
-        <View style={{ alignItems: 'center', marginTop: 10}}>
+        <View style={{marginTop: 10}}>
 
           <Text>Welcome back, {user.email}!</Text>
-
-          {myPosts.map((post, i) => <Post key={i} post={post} postKey={i}/>)}
-
+          
           <Button 
             title="Create a new post"         
-            style={{ marginVertical: 50 }}
+            style={{ marginVertical: 15 }}
             onPress={createNewPost}
           />
+
+          <View 
+            style={{ marginVertical: 20, justifyContent: 'center', alignItems: 'center'}}
+          >
+            <Modal 
+              visible={showCreateNewPostModal}
+              animationType="slide"
+            >
+              <CreatePostModal onPost={handlePost} postToInsert={handleGetPost} />
+            </Modal>
+
+          </View>
+          {myPosts.map((post, i) => <Post key={i} post={post} postKey={i} postToDelete={getPostToDelete} />)}
         </View> 
       </ScrollView>
     );
   }
 };
-// <Post post={post} key={i} />))
 export default MyDeskScreen;
