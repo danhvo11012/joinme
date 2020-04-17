8d32446a4dcd52c0bae3cad68afe88ba71df363b
danhvo11012 <danhvo11012@gmail.com>
2020-04-17 00:56:55 -0500

    Added user Avatar on commenttextbox

--- joinme/components/Post.js
+++ joinme/components/Post.js
@@ -268,7 +268,7 @@ function Post(props) {
           <CommentTextBox 
             post={props.post}
             currentUserId={props.currentUserId}
-            // userAvatar={props.currentUserAvatar}
+            userAvatar={profile.avatar}
             getCommentCallback={getCommentFromCommentTBox}
           />
       </Card>
